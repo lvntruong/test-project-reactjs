6e908f800dd17d6ca346155bf96b3a8ea03478be
Truong (Troy) V.N. LE <nhattruong0210@hotmail.com>
2020-08-04 13:40:45 +0700

    Add axios to backend, handle frontend remove index

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const bodyParser = require('body-parser')
 const app = express()
 const path = require('path')
 const httpServer = http.createServer(app)
+const cors = require('cors')
+const axios = require('axios')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(cors())
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'))
@@ -15,5 +18,20 @@ if (process.env.NODE_ENV === 'production') {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
 }
+
+app.get("/", (req, res) => {
+    res.set('Access-Control-Allow-Origin', '*');
+    axios.get('https://jsonplaceholder.typicode.com/comments')
+    .then(function (response) {
+        res.json(response.data)
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
+});
 
 httpServer.listen(process.env.PORT || 3001)
